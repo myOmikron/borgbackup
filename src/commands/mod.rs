bd6e0b24e18133a295542b12463c7941778cbe0c
myOmikron <git@omikron.dev>
2023-03-21 04:20:27 +0100

    Added borg prune command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,7 +6,11 @@ pub use create::{create_async_progress, CreateProgress};
 #[cfg(feature = "tokio")]
 pub use init::init_async;
 pub use init::{init, InitError, InitOptions};
+#[cfg(feature = "tokio")]
+pub use prune::prune_async;
+pub use prune::{prune, PruneError, PruneOptions, PruneWithin, PruneWithinTime};
 
 pub mod common;
 mod create;
 mod init;
+mod prune;
